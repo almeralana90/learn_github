51b1af0b8f7c21d2361c22294701dcf046f199c1
almeralana90 <almer.learn@gmail.com>
2023-01-02 13:41:33 +0700

    this is first commit

---
+++ function.py
@@ -0,0 +1,17 @@
+
+
+def grossrevenue(price, quantity):
+    return price * quantity
+
+a = int(input("masukan harga = "))
+b = int(input("masukan quantity barang = "))
+
+total = grossrevenue(a,b)
+print(f"total transaksi adalah {total} rupiah")
+
+def multiply(c,d):
+    return c * d
+
+
+    
+
